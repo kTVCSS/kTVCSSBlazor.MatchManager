f614d7dc4fdca4537d552ee8ccc077409bda8aec
kurwanator1337 <ktvcss@mail.ru>
2025-07-31 21:52:02 +0400

    Добавлены стандартные фичи для бэкенд проекта ktvcss

--- kTVCSSBlazor.MatchManager/Program.cs
+++ kTVCSSBlazor.MatchManager/Program.cs
@@ -1,5 +1,45 @@
+using kTVCSSBlazor.Db;
+using kTVCSSBlazor.MatchManager.Services;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSignalR(options =>
+{
+    options.EnableDetailedErrors = true;
+    options.KeepAliveInterval = TimeSpan.FromSeconds(15);
+    options.ClientTimeoutInterval = TimeSpan.FromSeconds(30);
+});
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.JsonSerializerOptions.WriteIndented = true;
+        options.JsonSerializerOptions.NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals;
+    });
+
+builder.Services.AddSingleton<IRepository, kTVCSSRepository>();
+builder.Services.AddDbContext<EFContext>(ServiceLifetime.Singleton);
+builder.Services.AddDbContextFactory<EFContext>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("OnlyDevkTVCSS", builder =>
+    {
+        builder
+            .WithOrigins("http://localhost:5001", "https://dev.ktvcss.ru", "https://ktvcss.ru")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("OnlyDevkTVCSS");
+
+app.UseDomainRestriction(["dev.ktvcss.ru", "localhost:5001"]);
+
+app.UseStaticFiles(new StaticFileOptions() { ServeUnknownFileTypes = true, DefaultContentType = "application/octet-stream" });
 
 app.Run();
---
+++ kTVCSSBlazor.MatchManager/Services/DomainRestrictionMiddleware.cs
@@ -0,0 +1,65 @@
+﻿namespace kTVCSSBlazor.MatchManager.Services
+{
+    public class DomainRestrictionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly List<string> _allowedDomains;
+
+        public DomainRestrictionMiddleware(RequestDelegate next, List<string> allowedDomains)
+        {
+            _next = next;
+            _allowedDomains = allowedDomains;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var host = context.Request.Headers.Host.ToString();
+            var origin = context.Request.Headers.Origin.ToString();
+
+#if DEBUG
+            Console.WriteLine(context.Request.Path);
+
+            foreach (var header in context.Request.Headers)
+            {
+                Console.WriteLine("header - " + header.Key + " - " + header.Value.ToString());
+            }
+#endif
+
+            if (context.Request.Path.HasValue)
+            {
+                if (context.Request.Path == "/api/RequestUpdateTotalPlayers")
+                {
+                    await _next(context);
+                    return;
+                }
+            }
+
+            bool allow = false;
+
+            foreach (var domain in _allowedDomains)
+            {
+                if (host.Contains(domain) || (!string.IsNullOrEmpty(origin) && origin.Contains(domain)))
+                {
+                    allow = true;
+                    await _next(context);
+                    break;
+                }
+            }
+
+            if (!allow)
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsync("403: Not allowed");
+            }
+        }
+    }
+
+    public static class DomainRestrictionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseDomainRestriction(
+            this IApplicationBuilder builder, List<string> allowedDomains)
+        {
+            return builder.UseMiddleware<DomainRestrictionMiddleware>(allowedDomains);
+        }
+    }
+}
